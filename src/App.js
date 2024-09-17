cdc73193434d7746cf5f0188c2c32eecfb0ac5c1
Svetlana <medjog@gmail.com>
2024-09-17 10:22:09 +0400

    homework 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+import React from 'react';
+import Message from './components/Message';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Сообщения</h1>
+      <Message text="Привет, это сообщение!" />
+      <Message text="Другой текст сообщения." />
+      <Message text="Новый текст сообщения." />
     </div>
   );
 }
